fb5f97e4fa2e568276af23927ee0cfa176aefe36
Alice Duchess <The-Duchess@users.noreply.github.com>
2015-07-10 02:27:43 -0700

    Update exampleplugin.rb

--- exampleplugin.rb
+++ exampleplugin.rb
@@ -6,17 +6,33 @@
 require 'rirc.rb'
 
 class Template < Pluginf
-	#any functions you may need
+	# any functions you may need can be included here
 
-	#your definition for function called if the regex for the plugin matches the message.message
+	# your definition for function called if the regex for the plugin matches the message.message
 	# inputs:
 	# 	- IRC_message object
 	# 	- admins array
 	# 	- backlog array of IRC_message objects
+	# output: string to send to the socket
 	def script(message, admins, backlog)
 
 		# plugins must return the raw mesaage they wish to have sent to the socket
-		return "PRIVMSG #{message.chan} :hello"
+		# return "PRIVMSG #{message.chan} :hello"
+		# or you can use functions to simplify this
+		# some are provided below
+		return privmsg(message.chan, "hello")
+	end
+	
+	def privmsg(dest, message)
+		return "PRIVMSG #{dest} :#{message}"
+	end
+	
+	def action(dest, message)
+		return "PRIVMSG #{dest} :\01ACTION\07\01 #{message}"
+	end
+
+	def notice(dest, message)
+		return "NOTICE #{dest} :#{message}"
 	end
 end
 
@@ -33,5 +49,5 @@ fn = "file name" # file name
 na = "template" # name for plugin
 de = "NOTES ^| HELP" # description and or help
 
-#plugin = Class_name.new(regex, name, file_name, help)
+# plugin = Class_name.new(regex, name, file_name, help)
 $temp_plugin = Template.new(reg_p, na, fn, de)
