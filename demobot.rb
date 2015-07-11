47e1126b288e7406b7a38496e6257b852c9d18d4
Isaac Archer <archeri@cat.pdx.edu>
2015-07-10 22:38:59 -0700

    update

--- demobot.rb
+++ demobot.rb
@@ -66,6 +66,14 @@ until bot.socket.eof? do
 
 		if msg.message_regex(/^hello/) then bot.privmsg(msg.channel, "hello #{msg.nick}"); next; end
 
+		if msg.message_regex(/^`info$/)
+			bot.privmsg(msg.nick, "this is a demo irc bot build using the rirc framework")
+			bot.privmsg(msg.nick, "the rirc framework is an irc bot framework built in ruby")
+			bot.privmsg(msg.nick, "the rirc framework is developed by Alice Archer")
+			bot.privmsg(msg.nick, "the rirc framework is published under the MIT license")
+			bot.privmsg(msg.nick, "the rirc framework is available here: https://github.com/The-Duchess/ruby-irc-framework")
+		end
+
 		if msg.message_regex(/^`part /)
 			tokens = msg.message.split(" ")
 			reason = ""
