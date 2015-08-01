901cc4f08cb19da3dd6176271bc93da4772b574e
Isaac Archer <archeri@cat.pdx.edu>
2015-07-31 18:33:37 -0700

    update

--- demobot2.rb
+++ demobot2.rb
@@ -42,8 +42,8 @@ end
 # the arguments you have are the channel and the command from the IRC_message
 bot.on :command do |chnl, cmd|
       case |chnl|
-      when /#{bot.nick_name}$/ and cmd == 'KICK' and auto_rejoin
-            bot.join(chnl.split(" ")[0].to_s)
+      when /#{bot.nick_name}$/
+            if cmd == 'KICK' and auto_rejoin then bot.join(chnl.split(" ")[0].to_s) end
       end
 end
 
