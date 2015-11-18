fcf368183bf73ac421dac291f85d5732af73753f
Isaac Archer <archeri@cat.pdx.edu>
2015-11-17 20:07:26 -0800

    adding client

---
+++ ircclient.rb
@@ -0,0 +1,52 @@
+#! /usr/bin/env ruby
+
+require_relative 'rirc.rb' # this line must be at the top of the file
+
+network = "irc.freenode.net"
+port = 6697
+pass = ""
+nick = "yoth"
+username = "rircbot"
+realname = "rircbot"
+nickserv_pass = ""
+channels = ["#lainchan"]
+admins = ["yoth"]
+use_ssl = true
+use_pass = false
+
+bot = IRCBot.new(network, port, nick, username, realname)
+bot.set_admins(admins)
+
+bot.on :message do |msg|
+      puts "[#{msg.channel}] <#{msg.nick}>: #{msg.message}"
+end
+
+bot.setup(use_ssl, use_pass, pass, nickserv_pass, channels)
+bot_thread = Thread.new { bot.start! }
+#bot_thread.run
+#bot_thread.join
+
+main_thread = Thread.new {
+
+      while true
+            input = STDIN.gets
+            channel = input.split(" ")[0].to_s
+            output = ""
+            1.upto(input.split(" ").length - 1) do |i|
+                  output.concat("#{input.split(" ")[i]} ")
+            end
+            output = output[0..-2].to_s
+            bot.privmsg(channel, output)
+      end
+
+}
+
+while true
+      sleep 0.001
+      bot_thread.join(0.1)
+      sleep 0.001
+      main_thread.join(0.1)
+end
+
+#main_thread.run
+#main_thread.join
